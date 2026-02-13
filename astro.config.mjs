9cdc94785779246a373d12fb3ba83e8c3c97fb3d
Ratama Adhi <ratama.adhi@gmail.com>
2026-02-13 19:32:02 +0700

    feat(ui): add theme toggle and greeting component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://try-astro.ratama-adhi.workers.dev"
+  site: "https://try-astro.ratama-adhi.workers.dev",
+  integrations: [preact()]
 });
